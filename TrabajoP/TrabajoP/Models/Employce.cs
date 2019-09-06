1c2f7f70596a36cca444988bee642ad73911bed9
Enrique Urcullo <enriquencido@gmail.com>
2019-09-06 17:59:07 -0400

    segundo commit

---
+++ TrabajoP/TrabajoP/Models/Employce.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrabajoP.Models
+{
+    public class Employce
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string birthday{ get; set; }
+        public int Salary { get; set; }
+    }
+}
